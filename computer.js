199bf32f9820b5a81d957e01215b8477be2ee879
elizaatt17 <85302513+elizaatt17@users.noreply.github.com>
2021-07-21 13:33:07 +0530

    Add files via upload

---
+++ computer.js
@@ -0,0 +1,23 @@
+constructor(){
+    this.life1="green";
+    this.life2="green";
+    this.life3="green";
+
+
+}
+life1(){
+    push();
+    textSize(20) ;
+    fill(white);
+    text("player",280,40) ;
+    
+    fill(this.life1);
+    rect(180,50,70,30);
+    fill(this.life2);
+    rect(250,50,70,30);
+    fill(this.life3);
+    rect(320,50,70,30);
+    pop();
+    
+        
+    }
